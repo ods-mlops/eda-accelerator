c87367c398a940449e91e342bfeff19e73a3467f
ods-mlops <ods-mlops>
2025-01-02 16:07:12 +0000

    Data Profiling Notebook
    Committing the Data Profiling Notebook

---
+++ Data Profiling.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC The Data Profiling notebook takes the output DataFrame from the 'Data Load' notebook. This notebook will explorer the Data Types, Summary Statistics, Data Completeness, Uniqueness and Cardinality, Duplication, Value Distribution Analysis. The notebook can be similar to a script if the your data is loaded into a DataFrame named 'df'.
+
+# COMMAND ----------
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# COMMAND ----------
+
+df.shape
+
+# COMMAND ----------
+
+df.info()
